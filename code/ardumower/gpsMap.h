670db90b7db2954f6a0a5eaff3b959633cb25600
Dysp1 <lord.dysp@gmail.com>
2020-06-14 16:37:09 +0300

    GPSMAP mergee

--- code/ardumower/gpsMap.h
+++ code/ardumower/gpsMap.h
@@ -18,41 +18,67 @@
   You are free to use the code in this classs for commercial or personal use at your will.
 */
 
-
-
 #ifndef gpsMap_h
 #define gpsMap_h
 
 #include "Arduino.h"
 
-#define MAXMAINAREAPOINTS 31
+#define MAXMAINAREAS 4
+#define MAXEXCLUSIONAREAS 10
+
+#define MAXMAINAREAPOINTS 31 // 30 + 1 extra. last point must be = first point
+#define MAXEXCLUSIONAREAPOINTS 11 // 10 + 1 extra. last point must be = first point
+
 #define MAGIC 1
 #define ADDR_GPSMAP_DATA 2049
 
 typedef struct {float x, y;} Point;
 
+typedef struct  {
+                  int numPoints = 0; 
+                  Point point[MAXEXCLUSIONAREAPOINTS] = {};
+                } mainArea;
+
+typedef struct  {
+                  int numPoints = 0; 
+                  Point point[MAXEXCLUSIONAREAPOINTS] = {};
+                } exclusionArea;
+
 class gpsMap
 {
     private:
+        mainArea _mainAreas[MAXMAINAREAS];
+        exclusionArea _exclusionAreas[MAXEXCLUSIONAREAS];
+
+        int _numberOfMainAreas = 1; // we must have at least one main area
+        int _numberOfExclusionAreas = 1;
+
         int isLeft( Point P0, Point P1, Point P2 );
+
         Point _mainAreaPointList[MAXMAINAREAPOINTS] = {};
         uint8_t _numberOfMainAreaPoints = 0;
         
-        Point _longGrassTempArea[5] = {{0.0,0.0},{0.0,0.0},{0.0,0.0},{0.0,0.0},{0.0,0.0}};
-        bool _longGrassTeamAreaInUse = 0;
+        Point _longGrassTempArea[5] = {};
+        bool _longGrassTeamAreaInUse;
         float _tempAreaStartTime;
         float _tempAreaSize = 0.00020;
         float _tempAreaTimeIfNoLongGrassFound = 300000;
     public:
+        void doUnitTest(); 
         void loadSaveMapData(boolean readflag);
         int insidePerimeter(float x, float y);
         float distanceToClosestWall(float x, float y);
-        uint8_t addMainAreaPoint( float x, float y);
+        int addMainAreaPoint(int areaNum, float lat, float lon);
         uint8_t removeMainAreaPoint( int pointNro);
-        float getMainAreaPointX(int pointNro);
-        float getMainAreaPointY(int pointNro);
-        uint8_t getNumberOfMainAreaPoints();
+        float getMainAreaPointX(int areaNumber, int pointNumber);
+        float getMainAreaPointY(int areaNumber, int pointNumber);
+        int getNumberOfMainAreaPoints(int areaNumber);
         uint8_t setTemporaryArea( float x, float y);
+        int addExclusionAreaPoint(int areaNum, float lat, float lon);
+        float getExclusionAreaPointX(int areaNumber, int pointNumber);
+        float getExclusionAreaPointY(int areaNumber, int pointNumber);
+        int getNumberOfExclusionAreaPoints(int areaNumber);
+        //int removeExclusionAreaPoint( int pointNro);
 
 };
 
